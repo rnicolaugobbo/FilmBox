21a477aa0a22da0ecede12c63f7cf51148569036
Rafael Nicolau <rafaelnicolau@Rafaels-Mac-Pro.local>
2023-10-17 18:23:07 +0200

    Add logic after the first movie is added

--- FilmBox.py
+++ FilmBox.py
@@ -1,3 +1,5 @@
+import csv
+
 print("""
   ______ _ _           ____            
  |  ____(_) |         |  _ \           
@@ -14,7 +16,12 @@ movies_and_ratings = {}
 first_use = True
 is_active = True
 user_input_yes_no = ""
+user_input = ""
 user_name = ""
+add_movie = True
+show_list = True
+clear_list = True
+export = True
 
 def add_comment(movie_name):
   user_input_comment = input("Write a comment about the movie: ")
@@ -27,6 +34,8 @@ def add_movie_and_rating():
   else:
     print("Movie is already on your list!")
   user_input_rating = input("Please give {} a rating from 1 to 5 stars: ".format(user_input_movie))
+  while user_input_rating not in ["1", "2", "3", "4", "5"]:
+    user_input_rating = input("Please type a number from 1 to 5: ")
   movies_and_ratings[user_input_movie]["rating"] = user_input_rating
 
   add_comment(user_input_movie)
@@ -34,15 +43,43 @@ def add_movie_and_rating():
 def show_list():
   print(movies_and_ratings)
 
+def clear_list():
+  movies_and_ratings.clear()
+
 while first_use:
   user_name = input("Please type your name: ")
   user_input_yes_no = input("Hello {}! Do you want to add your first movie to the list? (\"yes\" or \"no\"): ".format(user_name))
+  user_input_yes_no.lower()
   while user_input_yes_no not in ["yes", "no"]:
     user_input_yes_no = input("Please type \"yes or \"no\": ")
-  first_use = False
+  
+  if user_input_yes_no == "no":
+    print("Closing app...")
+    exit()
+  else:
+    first_use = False
 
 while is_active:
   add_movie_and_rating()
   print("Movie added to your list!")
 
+  user_input = \
+    input("Now that you have movie(s) in your list, you can: type \"1\" to see your list, type \"2\" to add another movie, type \"3\" to clear your list, \
+type \"4\" to export your list as a CSV file or type \"5\" to leave the app")
   
+  if user_input == "1":
+    show_list()
+  elif user_input == "2":
+    add_movie_and_rating()
+    print("Movie added to your list!")
+  elif user_input == "3":
+    clear_list()
+  elif user_input == "4":
+    fields = ["Film", "Rating", "Comment"]
+
+    with open("films_rating_and_comments.csv", "w") as films_csv:
+      doc_writer = csv.DictWriter(films_csv, fieldnames=fields)
+      doc_writer.writeheader()
+
+      for item in movies_and_ratings:
+        doc_writer.writerow(item)
