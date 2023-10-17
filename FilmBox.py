14c3f5bde9196a175ff982d7fc9aad6ce99dbfc0
Rafael Nicolau <rafaelnicolau@Rafaels-Mac-Pro.local>
2023-10-17 16:53:38 +0200

    Define variables

--- FilmBox.py
+++ FilmBox.py
@@ -1,1 +1,15 @@
+print("""
+  ______ _ _           ____            
+ |  ____(_) |         |  _ \           
+ | |__   _| |_ __ ___ | |_) | _____  __
+ |  __| | | | '_ ` _ \|  _ < / _ \ \/ /
+ | |    | | | | | | | | |_) | (_) >  < 
+ |_|    |_|_|_| |_| |_|____/ \___/_/\_\
+                                       
+                                       
+""")
 print("Welcome to FilmBox!")
+movies_and_ratings = {}
+first_use = True
+is_active = True
+user_input = ""
